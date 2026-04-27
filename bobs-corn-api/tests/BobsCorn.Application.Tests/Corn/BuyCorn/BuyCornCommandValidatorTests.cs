68926e8520205dffe67621d576186d83b08dbcc9
AccountsStatements <edward_el_doc@hotmail.com>
2026-04-27 03:17:08 -0600

    test: add buy corn use case unit tests

---
+++ bobs-corn-api/tests/BobsCorn.Application.Tests/Corn/BuyCorn/BuyCornCommandValidatorTests.cs
@@ -0,0 +1,26 @@
+﻿using BobsCorn.Application.Corn.BuyCorn;
+using FluentAssertions;
+
+namespace BobsCorn.Application.Tests.Corn.BuyCorn
+{
+    public sealed class BuyCornCommandValidatorTests
+    {
+        private readonly BuyCornCommandValidator _validator = new();
+
+        [Fact]
+        public void Validate_ShouldReturnError_WhenClientIdIsRequired()
+        {
+            // Arrange
+            var command = new BuyCornCommand(string.Empty);
+
+            // Act
+            var result = _validator.Validate(command);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(error =>
+                error.PropertyName == nameof(BuyCornCommand.ClientId) &&
+                error.ErrorMessage == "ClientId is required.");
+        }
+    }
+}
