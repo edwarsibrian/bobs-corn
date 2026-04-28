5209a0ce33d7fa6c674827c31b695b30143afb97
AccountsStatements <edward_el_doc@hotmail.com>
2026-04-28 17:52:29 -0600

    feat: add corn purchase persistence

--- bobs-corn-api/src/BobsCorn.Infrastructure/Configurations/InfrastructureServiceRegistration.cs
+++ bobs-corn-api/src/BobsCorn.Infrastructure/Configurations/InfrastructureServiceRegistration.cs
@@ -1,7 +1,9 @@
 ﻿using BobsCorn.Application.Clock;
+using BobsCorn.Application.Corn.BuyCorn;
 using BobsCorn.Application.RateLimiting;
 using BobsCorn.Infrastructure.Common;
 using BobsCorn.Infrastructure.Persistence;
+using BobsCorn.Infrastructure.Persistence.Dapper;
 using BobsCorn.Infrastructure.RateLimiting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,10 +13,13 @@ namespace BobsCorn.Infrastructure.Configurations
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionString)
         {
+            // Database
             services.AddSingleton<IDbConnectionFactory>(_ => new SqlConnectionFactory(connectionString));
 
             services.AddSingleton<IClock, SystemClock>();
             services.AddSingleton<IRateLimiter, InMemoryCornRateLimiter>();
+
+            services.AddScoped<ICornPurchaseStore, CornPurchaseStore>();
 
             return services;
         }
---
+++ bobs-corn-api/src/BobsCorn.Infrastructure/Persistence/Dapper/CornPurchaseStore.cs
@@ -0,0 +1,46 @@
+﻿using BobsCorn.Application.Corn.BuyCorn;
+using Dapper;
+
+namespace BobsCorn.Infrastructure.Persistence.Dapper
+{
+    public sealed class CornPurchaseStore : ICornPurchaseStore
+    {
+        private readonly IDbConnectionFactory _dbConnection;
+
+        public CornPurchaseStore(IDbConnectionFactory dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public async Task<int> AddCornPurchaseAsync(string clientId, DateTimeOffset purchasedAtUtc, CancellationToken cancellationToken)
+        {
+            using var connection = _dbConnection.CreateConnection();
+
+            const string insertSql= """
+                INSERT INTO 
+                    CornPurchases (Id, ClientId, PurchasedAtUtc)
+                VALUES (@Id, @ClientId, @PurchasedAtUtc);
+            """;
+
+            const string countSql = """
+                SELECT 
+                    COUNT(1) 
+                FROM CornPurchases
+                WHERE 
+                    ClientId = @ClientId;
+            """;
+
+            await connection.ExecuteAsync(insertSql, new
+            {
+                Id = Guid.NewGuid(),
+                ClientId = clientId,
+                PurchasedAtUtc = purchasedAtUtc
+            });
+
+            return await connection.ExecuteScalarAsync<int>(countSql, new
+            {
+                ClientId = clientId
+            });
+        }
+    }
+}
